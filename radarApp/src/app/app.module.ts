7a24a83ac34d77062609e4327d2b6bc718769fcf
Dorly Junior <dorly.junior@autoglass.com.br>
2021-08-26 17:33:57 -0300

    Adiciona remoção de item do carrinho, favoritos e localização de preço

--- radarApp/src/app/app.module.ts
+++ radarApp/src/app/app.module.ts
@@ -7,7 +7,12 @@ import { CarrinhoComponent } from './components/carrinho/carrinho.component'
 import { CarrinhoTotalComponent } from './components/carrinho-total/carrinho-total.component'
 import { ProdutoCardComponent } from './components/produto-card/produto-card.component'
 import { HttpClientModule } from '@angular/common/http'
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+
+import { LOCALE_ID } from '@angular/core';
+import localePt from '@angular/common/locales/pt';
+import {registerLocaleData} from '@angular/common';
+registerLocaleData(localePt)
 
 @NgModule({
     declarations: [
@@ -17,7 +22,10 @@ import { FormsModule } from '@angular/forms'
         ProdutoCardComponent,
     ],
     imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
-    providers: [],
+    providers: [{
+        provide: LOCALE_ID, 
+        useValue: "pt-BR"
+      }],
     bootstrap: [AppComponent],
 })
 export class AppModule {}
