402d390296ad06ffc7bd192f8864716d6a573ddc
Dorly Junior <dorly.junior@autoglass.com.br>
2021-08-27 09:43:28 -0300

    Adiciona passo 00

--- radarApp/src/app/app.module.ts
+++ radarApp/src/app/app.module.ts
@@ -3,16 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CarrinhoComponent } from './components/carrinho/carrinho.component';
-import { CarrinhoTotalComponent } from './components/carrinho-total/carrinho-total.component';
-import { ProdutoCardComponent } from './components/produto-card/produto-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    CarrinhoComponent,
-    CarrinhoTotalComponent,
-    ProdutoCardComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
